d22228a7e1476faba0f618f2f51450161b20f703
Sajratul Yakin Rubaiat <sajratul.rubaiat@enosisbd.com>
2023-07-25 00:48:05 +0600

    update path

--- main.py
+++ main.py
@@ -73,7 +73,7 @@ def get_chat_history(inputs) -> str:
 # Get the appropriate path type for your system
 
 key = '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'
-DATA_STORE_DIR = pathlib.Path("data_store")
+DATA_STORE_DIR = pathlib.Path("./data_store")
 
 
 # Load the LangChain.
